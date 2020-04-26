5d24766c5f4d793c7e99f031a8734be17f326729
Matteo Balzerani <matteo.balzerani@gmail.com>
2020-04-26 11:02:37 +0200

    improve log and error

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
 from flask import Flask, jsonify, abort, request
 
 import random
+import logging
 
 
 app = Flask(__name__)
 
+logger = logging.getLogger('werkzeug') #WSGI logger
+
 @app.route('/mock', methods=['POST'])
 def create_task():
+    logger.info("received {}".format(request))
+    logger.info("received generic json: {}".format(request.json))
     if not request.json or not 'payload' in request.json:
         abort(400)
+    logger.info("received json: {}".format(request.json))
     i = random.randint(1, 100)
     if (i>80):
-         abort(400)
+         abort(500)
     msg = {
         'payload': request.json['payload']
     }
